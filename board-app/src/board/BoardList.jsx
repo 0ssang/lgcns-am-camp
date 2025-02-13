60db0c117af042541eddef58cc39ad9d74083767
조영상 <51320595+0ssang@users.noreply.github.com>
2025-02-13 14:05:24 +0900

    환경변수 세팅 전

---
+++ board-app/src/board/BoardList.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import.meta.env.VITE_REST_API_SERVER_IP;
+
+export default function BoardList() {
+  const [datas, setDatas] = useState([]);
+
+  const navigate = useNavigate();
+
+  const rest_api_host = import.meta.env.VITE_REST_API_HOST;
+  const rest_api_port = import.meta.env.VITE_REST_API_PORT;
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+
+    axios
+      .get(`http://${rest_api_host}:${rest_api_port}/api/v2/board`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res);
+        res && res.data && setDatas(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.status === 401) {
+          alert("[인증 토큰 누락] 로그인 후 다시 시도해 주세요.");
+        } else if (err.status === 403) {
+          alert("[인증 토큰 오류] 로그인 후 다시 시도해 주세요.");
+        }
+        navigate("/");
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <h2>게시판 목록</h2>
+        <table className="board_list">
+          <colgroup>
+            <col width="15%" />
+            <col width="*" />
+            <col width="15%" />
+            <col width="20%" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th scope="col">글번호</th>
+              <th scope="col">제목</th>
+              <th scope="col">조회수</th>
+              <th scope="col">작성일</th>
+            </tr>
+          </thead>
+          <tbody>
+            {datas.length > 0 &&
+              datas.map((board) => (
+                <tr key={board.boardIdx}>
+                  <td>{board.boardIdx}</td>
+                  <td className="title">
+                    <Link to={`/detail/${board.boardIdx}`}>{board.title}</Link>
+                  </td>
+                  <td>{board.hitCnt}</td>
+                  <td>{board.createdDt}</td>
+                </tr>
+              ))}
+            {datas.length === 0 && (
+              <tr>
+                <td colSpan="4">조회된 결과가 없습니다.</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+        <Link to="/write" className="btn">
+          글쓰기
+        </Link>
+      </div>
+    </>
+  );
+}
---
+++ board/src/main/java/board/configuration/SecurityConfiguration.java
@@ -0,0 +1,63 @@
+package board.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import board.security.JwtRequestFilter;
+import board.service.CustomUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+    
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+            .requestMatchers("/loginProc", "/joinProc").permitAll()
+            .requestMatchers("/api/**").hasAnyRole("ADMIN", "USER")
+            .anyRequest().authenticated()
+        );
+        
+        http.formLogin(auth -> auth.disable());
+        
+        http.csrf(auth -> auth.disable());
+        
+        http.sessionManagement(auth -> auth.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        
+        http.logout(auth -> auth.disable());
+        
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        http.cors(Customizer.withDefaults());
+        
+        return http.build();
+    }
+    
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authManagerBuilder.userDetailsService(customUserDetailsService)
+                          .passwordEncoder(bCryptPasswordEncoder());
+        return authManagerBuilder.build();
+    }
+}
